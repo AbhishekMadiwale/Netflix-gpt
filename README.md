7f773803d1db46f6ad564bc03ec0fde8264fe0b3
Abhishek Madiwale <abhishek.madiwale@gmail.com>
2025-03-07 09:00:27 +0530

    Login page is ready with sign in and signup form

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Netflix GPT
 
+- Create react app
+- Configured TailwindCSS
+- Header
+- Login Form
+- Signup Form
+
+# Feature 
+- Login/ Sign Up
+    - Sign in / Sign up form
+    - redirect to browse page
+- Browse (after authentication) 
+    - Header
+    - Main movie
+        - Trailer in background 
+        - Title and description
+        - MovieSuggestions
+            - MovieLists * N
+- NetflixGPT
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,20 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  // State variable to change the state of form
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  // Function to change the sign up and sign in form
+  const toggleForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/04ef06cc-5f81-4a8e-8db0-6430ba4af286/web/IN-en-20250224-TRIFECTA-perspective_3a9c67b5-1d1d-49be-8499-d179f6389935_large.jpg"
           alt="background"
         />
       </div>
-      <form className="relative p-12 bg-black">
-        <input type="text" placeholder="Email address" className="p-2 m-2" />
-        <input type="password" placeholder="Enter password" className="p-2 m-2" />
-        <button className="p-4 m-4">Sign in</button>
+      <form className="w-4/12 absolute p-12  bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign in" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Enter full name"
+            className="p-4 my-2 w-full bg-gray-700 border-2 border-white rounded-lg bg-opacity-50"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email address"
+          className="p-4 my-2 w-full bg-gray-700 border-2 border-white rounded-lg bg-opacity-50"
+        />
+
+        <input
+          type="password"
+          placeholder="Enter password"
+          className="p-4 my-2 w-full bg-gray-700 border-2 border-white rounded-lg bg-opacity-50"
+        />
+        <button className="p-4 my-6 bg-red-700 rounded-lg w-full">
+          {isSignInForm ? "Sign in" : "Sign up"}
+        </button>
+        <h2 className="text-center opacity-70">OR</h2>
+        <button className="p-4 my-6 bg-gray-600 rounded-lg w-full bg-opacity-60">
+          Use a Sign code
+        </button>
+        <h2 className="text-center">
+          <Link className="underline">Forget password?</Link>
+        </h2>
+        <p className="py-5">
+          <Link className="underline cursor-pointer" onClick={toggleForm}>
+            {isSignInForm
+              ? "New to Netflix? Sign up"
+              : "Already registered ? Sign in Now"}
+          </Link>
+        </p>
       </form>
     </div>
   );
